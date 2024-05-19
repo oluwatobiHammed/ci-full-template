28ec0c63880f13530c67e4711c36c4860f50d31e
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-05-19 19:00:24 -0400

    created get_sales_data to get data string from the users

--- run.py
+++ run.py
@@ -31,3 +31,18 @@ data = sales.get_all_values()
 
 # Print the data
 print(data)
+
+def get_sales_data():
+    """
+    Get sales input from the users
+    """
+
+    print("Please enter sales data from the last market.")
+    print("Data should be 6 digit numbers, seperated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+
+get_sales_data()
