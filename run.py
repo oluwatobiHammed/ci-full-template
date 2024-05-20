d1d9b8c8a3dfeec863f3837b4074e111749f8631
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-05-19 20:07:30 -0400

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -36,16 +36,19 @@ def get_sales_data():
     """
     Get sales input from the users
     """
+    while True:
+         print("Please enter sales data from the last market.")
+         print("Data should be 6 digit numbers, seperated by commas.")
+         print("Example: 10,20,30,40,50,60\n")
 
-    print("Please enter sales data from the last market.")
-    print("Data should be 6 digit numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+         data_str = input("Enter your data here: ")
 
-    data_str = input("Enter your data here: ")
-
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
-
+         sales_data = data_str.split(',')
+         if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    return sales_data
+    
 def validate_data(values):
     """
     Indside the try, converts all string values to integers.
@@ -58,7 +61,24 @@ def validate_data(values):
             raise ValueError(f"Exactly 6 values is required, you provided {len(values)} ")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again")
+        return False
+    return True
 
 
+def update_sales_worksheet(data):
+    """
+    update sales worksheet, add new row with the list data provided
+    """
+    print("updating sales worksheet....\n")
 
-get_sales_data()
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+
+    print("sales worksheet updated successfully....\n")
+
+
+data = get_sales_data()
+
+sales_data = [(num) for num in data ]
+
+update_sales_worksheet(sales_data)
